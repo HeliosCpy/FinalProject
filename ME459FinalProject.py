f7dd8cadae8ffc05cfff8f38ad9e3c10f4b3c915
HeliosCpy <73025932+HeliosCpy@users.noreply.github.com>
2020-12-10 17:56:56 -0600

    Update ME459FinalProject.py
    slight changes

--- ME459FinalProject.py
+++ ME459FinalProject.py
@@ -7,7 +7,7 @@ import math
 
 def digitalReader(Img):
 
-    #matrix for defining the digits based on the layout of the graphic
+        #matrix for defining the digits based on the layout of the graphic
     DIGITS_LOOKUP = {
 	(1, 1, 1, 0, 1, 1, 1): 0,
 	(0, 0, 1, 0, 0, 1, 0): 1,
@@ -38,8 +38,8 @@ def digitalReader(Img):
     warpedCut=cv2.threshold(warpedCut,70,255,cv2.THRESH_BINARY)[1]
     warped=cv2.GaussianBlur(warped,(5,5),5)
     #warped=cv2.GaussianBlur(warped,(5,5),1)
-    cv2.imshow('Gauge Picture1', warpedCut)
-    cv2.waitKey(0)
+    #cv2.imshow('Gauge Picture1', warpedCut)
+    #cv2.waitKey(0)
     warped=cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)  #convert to blur
     warped=cv2.threshold(warped,0,255,cv2.THRESH_BINARY)[1]
     
@@ -54,19 +54,19 @@ def digitalReader(Img):
             remove.append(c)
             cv2.drawContours(mask,[c],-1,0,6)
     clean2=clean.copy()
-    cv2.drawContours(clean2, remove, -1, (255,255,0), 8)
-    cv2.imshow('Gauge ', clean2)
-    cv2.imshow('warped1 ', warpedCut)
+    #cv2.drawContours(clean2, remove, -1, (255,255,0), 8)
+    #cv2.imshow('Gauge ', clean2)
+    #cv2.imshow('warped1 ', warpedCut)
     warped = cv2.morphologyEx(warped, cv2.MORPH_OPEN, kernel)
     warped=cv2.bitwise_and(warped,warped,mask=mask)
-    cv2.imshow('Gauge Picture2', warped)
-    cv2.waitKey(0)
+    #cv2.imshow('Gauge Picture2', warped)
+    #cv2.waitKey(0)
 
     cnts=cv2.findContours(warped.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     cnts=im.grab_contours(cnts)
-    #cv2.drawContours(warped, cnts, -1, (0,255,0), 1)
-    #cv2.imshow('Gauge Picture', warped)
-    #cv2.waitKey(0)
+    cv2.drawContours(warped, cnts, -1, (0,255,0), 1)
+    cv2.imshow('Gauge Picture', warped)
+    cv2.waitKey(0)
     temp=[]
     for c in cnts:
         if cv2.contourArea(c)>800: #if the area is sufficently large then we have an important item
@@ -79,46 +79,55 @@ def digitalReader(Img):
     digits=[]
     w_max=0
     [x,y,w,h]=cv2.boundingRect(digit[0])
-    w_max=w
+    #w_max=w
+    w_max=55
+    #print(digit)
+    print([x,y,w,h], len(digit))
     origin=x
+    #for d in digit:
+    i=0
     for d in digit:
-        [x,y,w,h]=cv2.boundingRect(d)
-        cv2.rectangle(clean,(x+w-w_max,y),(x+w,y+h),(0,255,0),2)
+        [a,y,c,h]=cv2.boundingRect(d)
+        x=origin+w_max*i
+        #cv2.rectangle(clean,(x,y),(x+w_max,y+h),(0,50*i,0),2)
+        #cv2.rectangle(clean,(x+w-w_max,y),(x+w,y+h),(0,255,0),2)
         #print(d)
+        #roi = warped[y:y+h,x:x+w_max]
         roi = warped[y:y+h,x+w-w_max:x+w]#gets the rectangle around the digits
         #now to break the rectangle into smaller sections for comparing to the digits index
         #cv2.drawContours(clean,rect,-1,0,1)
         #print(roi[0])
         x=x+w-w_max
-        dW=int(.20*roi.shape[0])
+        dW=int(.175*roi.shape[0])
         #print(dW)
         dH=int(0.15*roi.shape[1])
         breakout=[  
-            ((x,y),(x+w_max,y+dH)), #top
+            ((x,y),(x+w_max,y+2*dH)), #top
             ((x,y),(x+dW,y+h//2)), #top left
             ((x+w_max-dW,y),(x+w_max,y+h//2)), #top right 
             ((x,y+h//2-dH//2),(x+w_max,y+h//2+dH//2)), #middle
             ((x,y+h//2),(x+dW,y+h)), #bottom left
             ((x+w_max-dW,y+h//2),(x+w_max,y+h)), #bottom right
-            ((x,y+h-dH),(x+w_max,y+h)), #bottom
+            ((x,y+h-2*dH),(x+w_max,y+h)), #bottom
             ]
         light = len(breakout)*[0] #makes a tuple of arrays of zeros
         #print(light)
-
+        i=i+1
         #print
         
         for (e,((x1,y1),(x2,y2))) in enumerate(breakout):
                 #print(e)
                 #print(((x1,y1),(x2,y2)))
-                cv2.rectangle(clean,(x1,y1),(x2,y2),(255,0,0),2)
+                cv2.rectangle(clean,(x1,y1),(x2,y2),(50*i,50*i,0),5-i)
                 sec=warped[y1:y2,x1:x2]
                 t=cv2.countNonZero(sec)
-                print(t)
+                #print(t)
                 A=(x2-x1)*(y2-y1)
                 if(t/A)>.5:
                     light[e]=1
         digit=DIGITS_LOOKUP[tuple(light)]
         digits.append(digit)
+        print(light)
         #    sum=cv2.countNonZero(sec)
 
     print(digits)
@@ -141,6 +150,8 @@ def sqFinder(Img):
     cnts=im.grab_contours(cnts)     #makes an array of all the contours from the edged image
     cnts = sorted(cnts, key=cv2.contourArea, reverse=True) #sorts the array of contours
 
+    cv2.imshow('Gauge Picture3', edged)
+    cv2.waitKey(0)
 
     for c in cnts:
         p=cv2.arcLength(c,True) #finds the perimeter for a closed shape , true=close shape
@@ -151,18 +162,21 @@ def sqFinder(Img):
             #if we find a square (4 corners) return that
             cnt=approx
             break
+
     warped=four_point_transform(Img,cnt.reshape(4,2)) #change the shape of the orginal edged matrix to fit the new desired image bounds
     edged=four_point_transform(edged,cnt.reshape(4,2))
-    warped=im.resize(warped, width=400)
+    warped=im.resize(warped, width=425)
     val=[warped,edged]
+
     cv2.imshow('Gauge Picture3', warped)
     cv2.waitKey(0)
+
     return val
 
 def main():
-    gaugeImg = cv2.imread('C:\\Users\\\OneDrive\\Desktop\\ME_459\\Project\\IMG_3672.JPEG')
-    digitalImg = cv2.imread('C:\\Users\\\OneDrive\\Desktop\\ME_459\\FinalProject\\ME459FinalProject\\testing.JPEG')
-    #digitalImg=im.resize(digitalImg, width=2500)
+    gaugeImg = cv2.imread('C:\\Users\\cftra\OneDrive\\Desktop\\ME_459\\Project\\IMG_3672.JPEG')
+    digitalImg = cv2.imread('C:\\Users\\cftra\OneDrive\\Desktop\\ME_459\\FinalProject\\ME459FinalProject\\testing.JPEG')
+    digitalImg=im.resize(digitalImg, width=2500)
     cv2.imshow('Gauge Picture3', digitalImg)
     cv2.waitKey(0)
     digitalReader(digitalImg)
